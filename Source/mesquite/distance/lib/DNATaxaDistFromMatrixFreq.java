e196169095dd958806ffe4bba83752cbe67c02e6
DMaddison <david.maddison@science.oregonstate.edu>
2006-05-03 17:54:24 +0000

    Moved TransitionDistance and Transversion distance to within PDistance Added &email and &tool to GenBank utilities Fixed colors for codon positions

--- Source/mesquite/distance/lib/DNATaxaDistFromMatrixFreq.java
+++ Source/mesquite/distance/lib/DNATaxaDistFromMatrixFreq.java
@@ -2,18 +2,18 @@ package mesquite.distance.lib;
 
 import mesquite.distance.lib.*;
 import mesquite.lib.*;
-import mesquite.lib.characters.MCharactersDistribution;
+import mesquite.lib.characters.*;
 import mesquite.categ.lib.*;
 
 public abstract class DNATaxaDistFromMatrixFreq extends DNATaxaDistFromMatrix {
 	MesquiteBoolean baseFreqEntireMatrix = new MesquiteBoolean(true);  //note:  Swofford uses this as true in PAUP*4.0b10
 	double[] pi = new double[4];
 	/*.................................................................................................................*/
-	public boolean startJob(String arguments, Object condition, CommandRecord commandRec, boolean hiredByName) {
-		super.startJob( arguments,  condition,  commandRec,  hiredByName);
+	public boolean superStartJob(String arguments, Object condition, CommandRecord commandRec, boolean hiredByName) {
+		super.superStartJob(arguments,  condition,  commandRec,  hiredByName);
 		for (int i = 0; i<4; i++)
 			pi[i]=0.25;
-		addCheckMenuItem(null, "Base Freq on Entire Matrix", MesquiteModule.makeCommand("toggleBaseFreqEntireMatrix", this), baseFreqEntireMatrix);
+		addCheckMenuItemToSubmenu(null,distParamSubmenu, "Base Freq on Entire Matrix", MesquiteModule.makeCommand("toggleBaseFreqEntireMatrix", this), baseFreqEntireMatrix);
 		return true;
   	 }	 
 	/*.................................................................................................................*/
@@ -43,7 +43,9 @@ public abstract class DNATaxaDistFromMatrixFreq extends DNATaxaDistFromMatrix {
 		return baseFreqEntireMatrix.getValue();
 	}
 	public double[] getPi(MCharactersDistribution observedStates, int it1, int it2){
-		DNAData data = (DNAData)observedStates.getParentData();
-		return data.getFrequencies(true, true, it1, it2);  
+		CharacterData data = observedStates.getParentData();
+		if (!(data instanceof DNAData))
+			return null;
+		return ((DNAData)data).getFrequencies(true, true, it1, it2);  
 	}
 }
--- Source/mesquite/molec/ReverseComplement/ReverseComplement.java
+++ Source/mesquite/molec/ReverseComplement/ReverseComplement.java
@@ -7,33 +7,33 @@ import mesquite.lib.table.*;
 import mesquite.molec.lib.*;
 
 public class ReverseComplement extends DNADataAltererCon {
- 	
-   	
+
+
 	public boolean alterBlockInTaxon(CharacterData data, int icStart, int icEnd, int it) {
 		if (data==null || !(data instanceof DNAData))
 			return false;
 		((DNAData)data).reverseComplement(icStart, icEnd, it, false, true);
 		return true;
 	}
-  
+
 	public String getName() {
 		return "Reverse Complement";
 	}
-	
+
 	public boolean requestPrimaryChoice() {
 		return true;
 	}
 
 	/*.................................................................................................................*/
- 	/** returns the version number at which this module was first released.  If 0, then no version number is claimed.  If a POSITIVE integer
- 	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
- 	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
-    	public int getVersionOfFirstRelease(){
-    		return 107;  
-    	}
-    	/*.................................................................................................................*/
-    	public boolean isPrerelease(){
-    		return true;
-    	}
+	/** returns the version number at which this module was first released.  If 0, then no version number is claimed.  If a POSITIVE integer
+	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
+	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
+	public int getVersionOfFirstRelease(){
+		return 107;  
+	}
+	/*.................................................................................................................*/
+	public boolean isPrerelease(){
+		return false;
+	}
 
 }
