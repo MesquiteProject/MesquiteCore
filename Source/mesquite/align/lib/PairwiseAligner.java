0b7b40c90ac8e0c10325ee7698f117cdc1a49561
Travis J Wheeler <travis@traviswheeler.com>
2006-05-03 20:50:58 +0000

    New class to hold common routines of the linear and quadratic classes. Fixing linear space with "keep gaps" (not done yet)

--- Source/mesquite/align/lib/PairwiseAligner.java
+++ Source/mesquite/align/lib/PairwiseAligner.java
@@ -17,10 +17,8 @@ public class PairwiseAligner  {
     //first gap char costs gapOpen + gapExtend, and each additional character costs gapExtend 
 	private int gapOpen;
 	private int gapExtend;
-	//private int alphabetSize;
 	private int subs[][];
 	private boolean keepGaps = false;
-//	private boolean useLowMem = false;
 	
 	public int totalGapChars = 0;
 		
@@ -32,16 +30,11 @@ public class PairwiseAligner  {
 	
 	private int alphabetLength=4;
 	private int gapInsertionArray[];
-	
-	private int lastAWhenBAligned[]; //lowMem alignment only
-	private int shapeWhenBAligned[];//lowMem alignment only
-	
+		
 	private boolean gapCostsInitialized = false;
 	private boolean subCostsInitialized = false;
+	private boolean seqsWereExchanged = false;
 		
-	
-	public boolean seqsWereExchanged=false;
-	
 	public PairwiseAligner (boolean keepGaps, int[][] subs, int gapOpen, int gapExtend, int alphabetLength) {
 		setSubsCostMatrix(subs);
 		setGapCosts(gapOpen, gapExtend);
@@ -86,15 +79,14 @@ public class PairwiseAligner  {
 		totalGapChars = preProcess(A_withGaps, B_withGaps);
 		
 		if ( returnAlignment) { 
-Debugg.println("length: " + (A.length*B.length));
-			if (A.length*B.length>getCharThresholdForLowMemory()) {
+			Debugg.println("length: " + (A.length*B.length));
+			if ((A.length*B.length)>getCharThresholdForLowMemory()) {
 				//low memory (but slower, due to recursion) alignment
 				AlignmentHelperLinearSpace helper = new AlignmentHelperLinearSpace(A, B, lengthA, lengthB, subs, gapOpen, gapExtend, alphabetLength);
-				lastAWhenBAligned = new int[lengthB +1];
-				shapeWhenBAligned  = new int[lengthB +1];
-				int myScore = recursivelyFillArray(helper, 0, lengthA, 0, lengthB, helper.noGap, helper.noGap);
-				long ret[][] = recoverAlignment(helper);
-//				Debugg.println("score is " + myScore);   //Travis:  generally we recommend using Debugg.println, as this is NOT intended to be present in released code, so it is easy to find
+				int myScore = helper.recursivelyFillArray(0, lengthA, 0, lengthB, helper.noGap, helper.noGap);
+				long ret[][] = helper.recoverAlignment(totalGapChars, seqsWereExchanged);
+				gapInsertionArray = helper.getGapInsertionArray();
+//							Debugg.println("score is " + myScore);   
 				if (ret.length>lengthA && ret.length>lengthB)
 					return stripEmptyBases(ret, MesquiteInteger.maximum(lengthA, lengthB));
 				
@@ -217,7 +209,6 @@ Debugg.println("length: " + (A.length*B.length));
 		 charThresholdForLowMemory = defaultCharThresholdForLowMemory;
 	}
 	
-
 	public void setIsMinimizationProblem (boolean isMin) {
 		isMinimize = isMin;
 	}
@@ -228,160 +219,6 @@ Debugg.println("length: " + (A.length*B.length));
 		return gapInsertionArray;
 	}			
 	
-	/* ************************************************* */
-	/* Everything below here is for lowMem alignment */
-	/* ************************************************* */
-	/** Private method which fills in portions of the full DP-table, then identifies for the middle row which column an optimal alignment must pass through
-	 * Fills shape array, which is used by recoverAlignment to find the actual alignment.  
-	 * */
-	private int recursivelyFillArray(AlignmentHelperLinearSpace helper, int firstColumn, int lastColumn, int firstRow, int lastRow, int precedingShape, int succeedingShape) {
-		
-		helper.fillArrays(firstRow, lastRow, firstColumn, lastColumn, precedingShape, succeedingShape );
-		
-		int midRow = (firstRow + lastRow)/2;
-		
-		int i, verticalColScore, diagonalColScore; 
-		int bestColScore = helper.bigNumber , bestCol = -1, bestColShape = helper.noGap;
-		for (i=firstColumn; i<=lastColumn; i++) {			
-			// best of the ways of leaving the i,j cell of the full DP table with a vertical edge 
-			verticalColScore = Math.min(helper.fH[i] + helper.rH[i] + gapExtend + gapOpen,
-							Math.min(helper.fH[i] + helper.rD[i] + gapExtend + gapOpen,
-							Math.min(helper.fH[i] + helper.rV[i] + gapExtend,
-							Math.min(helper.fD[i] + helper.rH[i] + gapExtend + gapOpen,
-							Math.min(helper.fD[i] + helper.rD[i] + gapExtend + gapOpen,
-							Math.min(helper.fD[i] + helper.rV[i] + gapExtend,			
-							Math.min(helper.fV[i] + helper.rH[i] + gapExtend,
-							Math.min(helper.fV[i] + helper.rD[i] + gapExtend,
-										helper.fV[i] + helper.rV[i] + gapExtend - gapOpen))))))));			
-
-			// best of the ways of leaving the i,j cell of the full DP table with a diagonal edge
-			if (i == lastColumn) { 
-				diagonalColScore = verticalColScore + 1; //TODO: this doesn't seem right. should it be "+subs[i][i+1]"?
-			} else {
-				int a = A[i];
-				int b = B[midRow];
-				int s = AlignUtil.getCost(subs,a,b,alphabetLength);
-				diagonalColScore = Math.min(helper.fH[i], Math.min (helper.fD[i], helper.fV[i])) +
-										Math.min(helper.rH[i+1], Math.min (helper.rD[i+1], helper.rV[i+1])) +
-										s;
-			}
-			// no need to track horizontal edges - those are already stored in the forward and reverse arrays
-			
-			if (verticalColScore < diagonalColScore) {
-				if ( verticalColScore < bestColScore) {
-					bestColScore = verticalColScore;
-					bestCol = i;
-					bestColShape = helper.gapInA;
-				}
-			} else {
-				if ( diagonalColScore < bestColScore) {
-					bestColScore = diagonalColScore;
-					bestCol = i;
-					bestColShape = helper.noGap;
-				}				
-			}
-		}
-		
-		lastAWhenBAligned[midRow] = bestCol;
-		shapeWhenBAligned[midRow] = bestColShape;
-				
-		//	Recurse to find the full list of cells through which the alignment passes.
-		if ( firstRow != midRow){
-			recursivelyFillArray(helper, firstColumn, bestCol, firstRow, midRow, precedingShape, bestColShape);
-		}
-		if (midRow+1 != lastRow){
-			if (bestColShape == helper.noGap){
-				recursivelyFillArray(helper, bestCol+1, lastColumn, midRow+1, lastRow, bestColShape, succeedingShape); 
-			} else {//gapInA
-				recursivelyFillArray(helper, bestCol, lastColumn, midRow+1, lastRow, bestColShape, succeedingShape);
-			}		
-		}
-		
-		return bestColScore; // useful for the first level of recursion - returns the total alignment cost
-	}
-	
-	/**  Private method: based on the shape array filled out by the recursive call above, determine the alignment 
-	 * */
-	private long[][] recoverAlignment (AlignmentHelperLinearSpace helper) {
-		int i=0,j, k=0;
-		
-		long[][] alignment = new long[lengthA + lengthB][2];
-		for (j = 0; j<lengthB; j++) {
-
-			//gap in B
-			while (i < lastAWhenBAligned[j]) {
-				alignment[k][0] = CategoricalState.expandFromInt(A[i]);
-				alignment[k][1] = CategoricalState.inapplicable;
-				i++;
-				k++;					
-			}
-
-			//now we're ready to burn off a letter from B, and possibly a letter from A if diagonal.
-			if (shapeWhenBAligned[j] == helper.noGap) {
-				alignment[k][0] = CategoricalState.expandFromInt(A[i]);
-				alignment[k][1] = CategoricalState.expandFromInt(B[j]);
-				i++;
-			} else {		// gapInA								
-				alignment[k][0] = CategoricalState.inapplicable;
-				alignment[k][1] = CategoricalState.expandFromInt(B[j]);
-			}
-			k++;
-		}
-
-		while (i < lengthA) {
-			alignment[k][0] = CategoricalState.expandFromInt(A[i]);
-			alignment[k][1] = CategoricalState.inapplicable;
-			i++;
-			k++;					
-		}		
-		
-		//trim off all the empty space at the end
-		long seq2return[][] = new long[k][2];
-		int ii=0; 
-		
-		for (i=0; i<k; i++) {
-			if (seqsWereExchanged) {//exhange the sequences
-				seq2return[i][0] = alignment[i][1];
-				seq2return[i][1] = alignment[i][0];				
-			} else {
-				seq2return[i][0] = alignment[i][0];
-				seq2return[i][1] = alignment[i][1];
-			}
-		}
-				
-		
-
-		if (keepGaps) {
-			long finalSeq2return[][] = new long[k+totalGapChars][2];
-			gapInsertionArray = new int[k+totalGapChars];
-			for(i=0; i<k+totalGapChars; i++) {
-				gapInsertionArray[i] =0;
-			}
-			
-			int usedGaps=0;
-			int recentGapRunLength=0;
-			j=0; // counts the number of letters in A seen so far
-			for (i=0; i<k; i++) {
-				if(seq2return[i][0] == CategoricalState.inapplicable) {
-					recentGapRunLength++;
-					gapInsertionArray[i+usedGaps]=1; //TODO: wrong
-				} else {
-					for (int m=0 ; m < followsGapSize[j]-recentGapRunLength; m++){
-						finalSeq2return[i+usedGaps][0] =  CategoricalState.inapplicable; 
-						finalSeq2return[i+usedGaps][1] =  CategoricalState.inapplicable; 
-						usedGaps++;
-					}
-					j++;
-					recentGapRunLength=0;
-				}				
-				finalSeq2return[i+usedGaps][0] = seq2return[i][0] ;
-				finalSeq2return[i+usedGaps][1] = seq2return[i][1] ;									
-			}		
-			return finalSeq2return;
-		} 
-		
-		return seq2return;
-	}
 	
 	/** This method returns true if better alignment scores are higher.  Override if needed. */
 	public boolean getHigherIsBetter() {
@@ -394,7 +231,7 @@ Debugg.println("length: " + (A.length*B.length));
 	}
 
 
-		/** This method returns the score of the worst possible match with the passed sequence.  Acceptable if result is approximate.  Override if needed. */
+	/** This method returns the score of the worst possible match with the passed sequence.  Acceptable if result is approximate.  Override if needed. */
 	public MesquiteNumber getVeryBadScore(long[] sequence, int oppositeLength, int alphabetLength, CommandRecord commandRec) {
 		MesquiteNumber score = new MesquiteNumber();
 		long[]opposite = new long[oppositeLength];
