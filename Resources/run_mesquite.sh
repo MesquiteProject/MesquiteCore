c4ff9ede7bf672c02b9d112aff36ef59d0e2f6a6
Danny Mandel <dmandel@tolweb.org>
2006-05-19 01:23:21 +0000

    incorporated travis' changes

--- Resources/run_mesquite.sh
+++ Resources/run_mesquite.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#Script to identify java VM and modify classpath on cygwin
+#Script to identify java VM. Also modifies classpath on cygwin.
 #Written by Travis Wheeler (www.traviswheeler.com)
 
-#If you find bugs or modify the script to improve functionality, 
+#If you find bugs or modify the script to improve functionality,
 # please let us know so we can improve the script for everyone
 
-
-# Thanks to William Cook (http://www.cs.utexas.edu/users/wcook/) for the bit that
-# identifies the path containing this script, and uses it to set the classpath. 
-# (the exact site of the work I based my code on is lost to antiquity)
-
+# Thanks to William Cook (http://www.cs.utexas.edu/users/wcook/) for the # bit that identifies the path containing this script, and uses it to
+# set the classpath. (the exact site of the work that served as
+# inspiration for this code is lost to antiquity) 
 
 #figure out where java lives 
 if [ $MESQUITE_JAVA_HOME ]
