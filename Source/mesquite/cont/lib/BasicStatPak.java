b761d729e04a4752ce8cb234e4d16f54336cfa3f
wmaddisn <wmaddisn@mail.ubc.ca>
2007-09-06 04:00:16 +0000

    Header comments changed to update copyright date and version number

--- Source/mesquite/cont/lib/BasicStatPak.java
+++ Source/mesquite/cont/lib/BasicStatPak.java
@@ -1,5 +1,5 @@
-/* Mesquite source code.  Copyright 1997-2006 W. Maddison and D. Maddison.
-Version 1.11, June 2006.
+/* Mesquite source code.  Copyright 1997-2007 W. Maddison and D. Maddison.
+Version 2.0, September 2007.
 This file copyright 2006 P. Midford and W. Maddison
 
 Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
