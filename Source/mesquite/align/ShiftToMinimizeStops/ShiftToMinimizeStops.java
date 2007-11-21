c559249c1ccb95e34a726b068cbf3c26fa4b2af6
DMaddison <david.maddison@science.oregonstate.edu>
2007-11-21 04:38:48 +0000

    Some updates to the manuals

--- Source/mesquite/align/ShiftToMinimizeStops/ShiftToMinimizeStops.java
+++ Source/mesquite/align/ShiftToMinimizeStops/ShiftToMinimizeStops.java
@@ -86,7 +86,7 @@ public class ShiftToMinimizeStops extends DNADataAlterer {
 	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
 	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
 	public int getVersionOfFirstRelease(){
-		return NEXTRELEASE;  
+		return -111;  
 	}
 	/*.................................................................................................................*/
 	public String getName() {
