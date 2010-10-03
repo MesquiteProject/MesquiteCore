638aecad9fb8a9d0ac11ea72e6af75bc5af7dec0
wmaddisn <wmaddisn@mail.ubc.ca>
2010-10-03 05:12:00 +0000

    Release 2.74 build 550, mostly header comments

--- Source/mesquite/lib/characters/CellBlock.java
+++ Source/mesquite/lib/characters/CellBlock.java
@@ -1,7 +1,7 @@
 package mesquite.lib.characters;
 
 /* Mesquite source code.  Copyright 1997-2010 W. Maddison and D. Maddison.
-Version 2.73, July 2010.
+Version 2.74, October 2010.
 Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
 The commenting leaves much to be desired. Please approach this source code with the spirit of helping out.
 Perhaps with your help we can be more than a few, and make Mesquite better.
--- Source/mesquite/trees/BranchLengthFromRoot/BranchLengthFromRoot.java
+++ Source/mesquite/trees/BranchLengthFromRoot/BranchLengthFromRoot.java
@@ -1,5 +1,5 @@
 /* Mesquite source code, Treefarm package.  Copyright 1997-2010 W. Maddison, D. Maddison and P. Midford. 
-Version 2.73, July 2010.
+Version 2.74, October 2010.
 Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
 The commenting leaves much to be desired. Please approach this source code with the spirit of helping out.
 Perhaps with your help we can be more than a few, and make Mesquite better.
