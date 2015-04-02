6ef162d92715b6b9e0c94082fbafec00672fbdce
wmaddisn <wmaddisn@mail.ubc.ca>
2015-04-02 14:39:02 -0700

    Setting build number, prerelease etc. for post-3.03

--- Source/mesquite/Mesquite.java
+++ Source/mesquite/Mesquite.java
@@ -42,7 +42,7 @@ public class Mesquite extends MesquiteTrunk
 	}
 	/*.................................................................................................................*/
 	public String getVersion() {
-		return "3.03";
+		return "3.03+";
 	}
 
 	/*.................................................................................................................*/
@@ -67,7 +67,7 @@ public class Mesquite extends MesquiteTrunk
 	}
 	/*.................................................................................................................*/
 	public boolean isPrerelease(){
-		return false;
+		return true;
 	}
 	/*.................................................................................................................*/
 	public void getEmployeeNeeds(){  //This gets called on startup to harvest information; override this and inside, call registerEmployeeNeed
