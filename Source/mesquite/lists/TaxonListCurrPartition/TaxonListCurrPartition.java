a0c107d0908fa0174b604ddf325026a2351ffabe
David Maddison <david.maddison@science.oregonstate.edu>
2016-11-04 16:35:30 -0700

    Contrasting colors used for Taxon Groups in TaxaList

--- Source/mesquite/lists/TaxonListCurrPartition/TaxonListCurrPartition.java
+++ Source/mesquite/lists/TaxonListCurrPartition/TaxonListCurrPartition.java
@@ -291,12 +291,13 @@ public class TaxonListCurrPartition extends TaxonListAssistant {
 		Color c = g.getColor();
 		MesquiteSymbol symbol = null;
 		boolean colored = false;
+		Color backgroundColor = null;
 		if (part!=null) {
 			TaxaGroup tg = part.getTaxaGroup(ic);
 			if (tg!=null){
-				Color cT = tg.getColor();
-				if (cT!=null){
-					g.setColor(cT);
+				backgroundColor= tg.getColor();
+				if (backgroundColor!=null){
+					g.setColor(backgroundColor);
 					g.fillRect(x+1,y+1,w-1,h-1);
 					colored = true;
 				}
@@ -321,10 +322,15 @@ public class TaxonListCurrPartition extends TaxonListAssistant {
 				return;
 			int sw = fm.stringWidth(s);
 			int sh = fm.getMaxAscent()+ fm.getMaxDescent();
-			if (selected)
-				g.setColor(Color.white);
-			else
-				g.setColor(Color.black);
+			if (backgroundColor==null) {
+				if (selected)
+					g.setColor(Color.white);
+				else
+					g.setColor(Color.black);
+			} else {  // background is color; choose contrasting color
+				Color contrast = ColorDistribution.getContrasting(backgroundColor);
+				g.setColor(contrast);
+			}
 			g.drawString(s, x+(w-sw)/2, y+h-(h-sh)/2);
 			if (c!=null) g.setColor(c);
 		}
