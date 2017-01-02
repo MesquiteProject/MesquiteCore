2ac20a007e4e46fb73b12c9bbf432e400365358a
David Maddison <david.maddison@science.oregonstate.edu>
2017-01-01 16:00:56 -0800

    All NEXTRELEASE changed to 320

--- Source/mesquite/charMatrices/AlteredMatrix/AlteredMatrix.java
+++ Source/mesquite/charMatrices/AlteredMatrix/AlteredMatrix.java
@@ -184,7 +184,7 @@ public class AlteredMatrix extends SourceModifiedMatrix {  //So that Altered Mat
 	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
 	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
 	public int getVersionOfFirstRelease(){
-		return NEXTRELEASE;  
+		return 320;  
 	}
 	/*.................................................................................................................*/
    	 public boolean showCitation(){
--- Source/mesquite/io/ExportDescriptions/ExportDescriptions.java
+++ Source/mesquite/io/ExportDescriptions/ExportDescriptions.java
@@ -144,7 +144,7 @@ public class ExportDescriptions extends FileInterpreterI {
 	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
 	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
 	public int getVersionOfFirstRelease(){
-		return NEXTRELEASE;  
+		return 320;  
 	}
 	/*.................................................................................................................*/
 	public String getName() {
