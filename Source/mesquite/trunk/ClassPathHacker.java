1be7b0281d3ae3ade8f4c7e6aa88a422d43e8ec3
David Maddison <david.maddison@science.oregonstate.edu>
2017-11-04 13:35:20 -0700

    Adding byte buddy agent

--- Source/mesquite/trunk/ClassPathHacker.java
+++ Source/mesquite/trunk/ClassPathHacker.java
@@ -4,6 +4,17 @@ package mesquite.trunk;
 import java.lang.reflect.*;
 import java.io.*;
 import java.net.*;
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.lang.ProcessHandle;
+import java.util.jar.JarFile;
+
+import mesquite.lib.MesquiteTrunk;
+
+import java.io.File;
+
+import net.bytebuddy.agent.ByteBuddyAgent;
+
 
 
 public class ClassPathHacker {
@@ -22,20 +33,42 @@ public class ClassPathHacker {
 	
 	public static void addURL(URL u) throws IOException {
 			
-		URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
-		Class sysclass = URLClassLoader.class;
-	
-		try {
-			Method method = sysclass.getDeclaredMethod("addURL",parameters);
-			method.setAccessible(true);
-			method.invoke(sysloader,new Object[]{ u });
-		} 
-		catch (Throwable t) {
-			t.printStackTrace();
-			throw new IOException("Error, could not add URL to system classloader");
+		if (MesquiteTrunk.isJavaGreaterThanOrEqualTo(9.0)) {
+			
+		} else
+		{
+			URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
+			Class sysclass = URLClassLoader.class;
+
+			try {
+				Method method = sysclass.getDeclaredMethod("addURL",parameters);
+				method.setAccessible(true);
+				method.invoke(sysloader,new Object[]{ u });
+			} 
+			catch (Throwable t) {
+				t.printStackTrace();
+				throw new IOException("Error, could not add URL to system classloader");
+			}
 		}
 					
 	}
 
+}
+
+
+class ClassPathAgent {
+    public static void agentmain(String args, Instrumentation instrumentation) throws IOException {
+        instrumentation.appendToSystemClassLoaderSearch(new JarFile(args));
+    }
+}
+
+
+
+class ClassPathUtil {
+    private static File AGENT_JAR = new File("/path/to/agent.jar");
+
+    public static void addJarToClassPath(File jarFile) {
+        ByteBuddyAgent.attach(AGENT_JAR, String.valueOf(ProcessHandle.current().pid()), jarFile.getPath());
+    }
 }
 
