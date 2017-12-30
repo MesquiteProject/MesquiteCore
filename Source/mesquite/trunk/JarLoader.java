ab756e0ce0491a5464b0e034400785c331e97cab
david <david@192.168.1.71>
2017-12-30 12:09:02 -0800

    Use of ByteBuddy

--- Source/mesquite/trunk/JarLoader.java
+++ Source/mesquite/trunk/JarLoader.java
@@ -14,21 +14,34 @@ import java.io.File;
 import net.bytebuddy.agent.ByteBuddyAgent;
 
 public class JarLoader {
+	static boolean noClassDefWarningGiven = false;
 
 	private static final Class[] parameters = new Class[]{URL.class};
 
-	public static void addJarFileToClassPath(String s) throws IOException {
-		File f = new File(s);
-		URI uri = f.toURI();
-		if (MesquiteTrunk.isJavaGreaterThanOrEqualTo(9.0)) {
-			Instrumentation instrumentation = ByteBuddyAgent.install();
-			instrumentation.appendToSystemClassLoaderSearch(new JarFile(f));
-		} else {
-			addURL(uri.toURL());
+	public static boolean addJarFileToClassPath(String s)  {
+		boolean jarAdded = false;
+		try {
+			File f = new File(s);
+			URI uri = f.toURI();
+			if (MesquiteTrunk.isJavaGreaterThanOrEqualTo(9.0)) {
+				Instrumentation instrumentation = ByteBuddyAgent.install();
+				instrumentation.appendToSystemClassLoaderSearch(new JarFile(f));
+				jarAdded = true;
+			} else {
+				addURL(uri.toURL());
+			}
+		} catch (IOException e) {
+			System.out.println("\nIOException\n");
 		}
+		catch (NoClassDefFoundError e) {
+			if (!noClassDefWarningGiven)
+				System.out.println("\nNoClassDefFoundError: ByteBuddy not found\n");
+			noClassDefWarningGiven= true;
+		}
+		return jarAdded;
 	}
-	
-	 static void addURL(URL u) throws IOException {
+
+	static void addURL(URL u) throws IOException {
 		URLClassLoader sysloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
 		Class sysclass = URLClassLoader.class;
 
