800e51615d03b35311056daa24ff555b0611d48f
wmaddisn <wmaddisn@mail.ubc.ca>
2018-01-06 16:29:11 -0800

    again

--- Source/mesquite/trunk/ModuleLoader.java
+++ Source/mesquite/trunk/ModuleLoader.java
@@ -58,7 +58,7 @@ public class ModuleLoader {
 		otherTime= new MesquiteTimer();
 		classTime = new MesquiteTimer();
 		verboseStartup = MesquiteFile.fileExists(MesquiteModule.getRootPath() + "verbose");
-		if (MesquiteTrunk.debugMode){
+		//if (MesquiteTrunk.debugMode){
 			if (MesquiteTrunk.basicClassLoader!= null){
 			System.out.println("Loading modules using classloader " + MesquiteTrunk.basicClassLoader);
 			if (MesquiteTrunk.basicClassLoader instanceof URLClassLoader){
@@ -73,7 +73,7 @@ public class ModuleLoader {
 			else
 				System.out.println("Loading modules: MesquiteTrunk.basicClassLoader is null");
 				
-		}
+	//	}
 		if (MesquiteTrunk.isApplet()) {
 			System.out.println("Error: attempt to use applet as application");
 		}
