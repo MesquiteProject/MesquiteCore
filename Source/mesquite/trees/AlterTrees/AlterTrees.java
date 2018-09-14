2eb4fca08bce706c341fd72251073c1d645c9dc1
David Maddison <david.maddison@science.oregonstate.edu>
2018-09-13 19:04:02 -0700

    Enabled notification if trees altered

--- Source/mesquite/trees/AlterTrees/AlterTrees.java
+++ Source/mesquite/trees/AlterTrees/AlterTrees.java
@@ -87,7 +87,7 @@ public class AlterTrees extends TreeListUtility {
 					CommandRecord.tick("Altering tree " + j + " of " + numTrees);
 			   		if (j % dotFreq == 0)
 			   			log(".");
-					boolean success = alterTask.transformTree((MesquiteTree)tree, null, false);
+					boolean success = alterTask.transformTree((MesquiteTree)tree, null, true); //WAYNECHECK: why was notify set to false here?  This caused a lack of updating in the tree list window.
  				}
 			}
 		}
