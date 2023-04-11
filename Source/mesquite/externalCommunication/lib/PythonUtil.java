87d4e83efa2731948654a3e989224cdbb122b891
David Maddison <david.maddison@science.oregonstate.edu>
2023-04-11 08:10:26 -0700

    getPythonPath

--- Source/mesquite/externalCommunication/lib/PythonUtil.java
+++ Source/mesquite/externalCommunication/lib/PythonUtil.java
@@ -5,6 +5,8 @@ import mesquite.lib.*;
 public class PythonUtil {
 	public static String python2Path = "";
 	public static String python3Path = "";
+	public static int pythonPreference = 3;
+	public static int NOPYTHONPREFERENCE = 0;
 
 	public static boolean pythonSettings(MesquiteModule ownerModule) {
 		MesquiteInteger buttonPressed = new MesquiteInteger(ExtensibleDialog.defaultCANCEL);
@@ -34,38 +36,47 @@ public class PythonUtil {
 	}
 
 	public static String pythonVersion(MesquiteModule ownerModule) {
-//		String pythonVersionStOut= ExternalProcessManager.executeAndGetStandardErr(this, "/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python", "-V");
-		
-//		String pythonVersionStOut= ExternalProcessManager.executeAndGetStandardErr(this, "python", "-V");
-/*		String raxmlng= ExternalProcessManager.executeAndGetStandardOut(this, "/usr/local/bin/raxml-ng", "-v");
+		//		String pythonVersionStOut= ExternalProcessManager.executeAndGetStandardErr(this, "/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python", "-V");
+
+		//		String pythonVersionStOut= ExternalProcessManager.executeAndGetStandardErr(this, "python", "-V");
+		/*		String raxmlng= ExternalProcessManager.executeAndGetStandardOut(this, "/usr/local/bin/raxml-ng", "-v");
 		logln("\nraxml-ng version: " + raxmlng);
 		String iqTreeV= ExternalProcessManager.executeAndGetStandardOut(this, "/usr/local/bin/iqtree2", "-V");
 		logln("\niq-tree version: " + iqTreeV);
-*/
-		
+		 */
 
-		
-/*		scriptRunner = new ShellScriptRunner(scriptPath, runningFilePath, null, true, getName(), outputFilePaths, this, this, true);  //scriptPath, runningFilePath, null, true, name, outputFilePaths, outputFileProcessor, watcher, true
+
+
+		/*		scriptRunner = new ShellScriptRunner(scriptPath, runningFilePath, null, true, getName(), outputFilePaths, this, this, true);  //scriptPath, runningFilePath, null, true, name, outputFilePaths, outputFileProcessor, watcher, true
 		success = scriptRunner.executeInShell();
 		if (success)
 			success = scriptRunner.monitorAndCleanUpShell(progressIndicator);
-*/
-		
+		 */
+
 		return null;
 
 	}
 
 	public static boolean python2Available() {
 		return StringUtil.notEmpty(python2Path);
-}
-	
-	
+	}
+
+
 	public static boolean python3Available() {
 		return StringUtil.notEmpty(python3Path);
-}
+	}
 
 	public static boolean pythonAvailable() {
 		return python2Available() || python3Available();
-}
+	}
+
+	public static String getPythonPath(int version) {
+		if (version==3 || (version==0 && pythonPreference==3))
+			return python3Path;
+		else if (version==2 || (version==0 && pythonPreference==2))
+			return python2Path;
+		else
+			return python3Path;
+	}
 
 }
