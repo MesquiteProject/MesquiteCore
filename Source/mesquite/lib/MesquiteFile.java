1ab8f4cd730f374326072cef02c7eecb007daea2
David Maddison <david.maddison@science.oregonstate.edu>
2024-04-08 20:17:24 -0700

    Using Desktop for showFolder

--- Source/mesquite/lib/MesquiteFile.java
+++ Source/mesquite/lib/MesquiteFile.java
@@ -3233,32 +3233,22 @@ public class MesquiteFile extends Listened implements HNode, Commandable, Listab
 	/*.................................................................................................................*/
 	/** Returns whether a directory can be shown in the OS's file browser." */
 	public static boolean canShowDirectory() {
-		return (MesquiteTrunk.isMacOSX() || MesquiteTrunk.isWindows());
+		return true; //(MesquiteTrunk.isMacOSX() || MesquiteTrunk.isWindows());
 	}
 	/*.................................................................................................................*/
-	/** Shows the directory specified in path in the Finder or Windows Explorer" */
+	/** Shows the directory specified in path in the Finder or Explorer" */
 	public static void showDirectory(String path) {
 		if (path == null)
 			return;
 		File file = new File(path);
 		if (!file.exists()|| !file.isDirectory())
 			return;
-		if (MesquiteTrunk.isMacOSX()) {
 
+		if (Desktop.isDesktopSupported()) {
 			try {
-				Runtime.getRuntime().exec(
-						new String[] { "open", file.getAbsolutePath() });
-			} catch (IOException e) {
-				e.printStackTrace();
+				Desktop.getDesktop().open(file);
 			}
-		} else {
-			File directoryPathFile = new File(path);
-			if (Desktop.isDesktopSupported()) {
-				try {
-					Desktop.getDesktop().open(directoryPathFile);
-				}
-				catch (IOException ex) {
-				}
+			catch (IOException ex) {
 			}
 		}
 
