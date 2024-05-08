8f46b4e75d77b707901366e0b9cfde100c3db029
wmaddisn <wmaddisn@192.168.1.74>
2024-05-08 16:15:21 -0700

    Adding gappiness filter to PhyIN

--- Source/mesquite/molec/PhyINSelector/PhyINSelector.java
+++ Source/mesquite/molec/PhyINSelector/PhyINSelector.java
@@ -27,10 +27,46 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 
 	MesquiteString xmlPrefs= new MesquiteString();
 	String xmlPrefsString = null;
+
+	//Primary PhyIN parameters
 	double proportionIncompat = 0.4;
-	int spanSize = 12;
+	int blockSize = 12;
 	int neighbourDistance = 1;
 	MesquiteBoolean treatGapAsState = new MesquiteBoolean(true);
+	// AAATAAAAAAACCAAAAAAAAAAA
+	// AAATTAAAAACCAAAAAAAAAAAA
+	// AAAATAAAAAAACAAAAAAAAAAA
+	// AAAAAAAAAACAAAAAAAAAAAAA
+	//    ><
+	//           ><
+	//            ><
+	//    ©©     ©©©
+	//    ssssssssssss
+	//    **********
+	// in the above, there are three pairs of conflicting neighbours (><). These pass the simple binary version of hte
+	// incompatibility criterion, in that all for combinations of states are present.
+	// Thus, there are 5 conflicted characters (©).
+	// Thus, within the stretch of 12 shown by s, there are 5/12 conflicted characters, thus > 0.4 proportion.
+	// Thus, the stretch within that from first to last of the conflicted characters is selected (*).
+
+
+
+	//Secondary gappiness trimming parameters
+	MesquiteBoolean filterGappiness = new MesquiteBoolean(true);
+	double siteGappinessThreshold = 0.5; // A site is considered good (for gappiness) if it is less gappy than this (term or non-term).
+	int minGappyBlockSize = 3; // If in a block of at least this many sites, the first and last site is bad,
+	double blockGappinessThreshold = 0.5; // and the proportion of bad sites is above this,
+	int minGappyBoundary = 4; // and there are no stretches of this many good sites in a row,
+	// then select the block.
+
+	// CCCCAA--A--A------TTTT--AACCCC
+	// CCCCAAG-A--A------TTTT--AACCCC
+	// CCCCAAG-A--A------TTTT--AACCCC
+	// CCCCAAG----A------------AACCCC
+	//        ***********
+	//
+	// in the above, the column after the Gs is the first bad site, and the two following As columns are too narrow to stop the block. However, the Ts
+	// columns are 4 in a row, so they stop the block
 
 	/*.................................................................................................................*/
 	public boolean startJob(String arguments, Object condition, boolean hiredByName) {
@@ -56,9 +92,14 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 		Snapshot temp = new Snapshot();
 
 		temp.addLine("setProportionIncompat " + proportionIncompat);
-		temp.addLine("setSpanSize " + spanSize);
-		temp.addLine("setSpanSize " + spanSize);
+		temp.addLine("setBlockSize " + blockSize);
+		temp.addLine("setSpanSize " + blockSize);
 		temp.addLine("setTreatGapAsState " + treatGapAsState.toOffOnString());
+		temp.addLine("setFilterGappiness " + filterGappiness.toOffOnString());
+		temp.addLine("setSiteGappinessThreshold " + siteGappinessThreshold);
+		temp.addLine("setMinGappyBlockSize " + minGappyBlockSize);
+		temp.addLine("setBlockGappinessThreshold " + blockGappinessThreshold);
+		temp.addLine("setMinGappyBoundary " + minGappyBoundary);
 		return temp;
 	}
 	/*.................................................................................................................*/
@@ -78,7 +119,7 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 		else if (checker.compare(this.getClass(), "Sets block size of PhyIN selection.", "[on or off]", commandName, "setSpanSize")) {
 			int s = MesquiteInteger.fromString(parser.getFirstToken(arguments));
 			if (MesquiteInteger.isCombinable(s)){
-				spanSize = s;
+				blockSize = s;
 				if (!MesquiteThread.isScripting())
 					parametersChanged(); 
 			}
@@ -89,6 +130,49 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 			treatGapAsState.toggleValue(parser.getFirstToken(arguments));
 			if (current!=treatGapAsState.getValue()) {
 				parametersChanged();
+			}
+		}
+		else if (checker.compare(this.getClass(), "Sets whether or not to filter by gappiness also.", "[on or off]", commandName, "setFilterGappiness")) {
+			boolean current = filterGappiness.getValue();
+			filterGappiness.toggleValue(parser.getFirstToken(arguments));
+			if (current!=filterGappiness.getValue()) {
+				parametersChanged();
+			}
+		}
+		else if (checker.compare(this.getClass(), "Sets minimum size of bad gappy block.", "[on or off]", commandName, "setMinGappyBlockSize")) {
+			int s = MesquiteInteger.fromString(parser.getFirstToken(arguments));
+			if (MesquiteInteger.isCombinable(s)){
+				minGappyBlockSize = s;
+				if (!MesquiteThread.isScripting())
+					parametersChanged(); 
+			}
+
+		}
+		else if (checker.compare(this.getClass(), "Sets required size to form a non-gappy boundary.", "[on or off]", commandName, "setMinGappyBoundary")) {
+			int s = MesquiteInteger.fromString(parser.getFirstToken(arguments));
+			if (MesquiteInteger.isCombinable(s)){
+				minGappyBoundary = s;
+				if (!MesquiteThread.isScripting())
+					parametersChanged(); 
+			}
+
+		}
+		else if (checker.compare(this.getClass(), "Sets proportion of taxa than need to have gaps for site to be gappy.", "[on or off]", commandName, "setSiteGappinessThreshold")) {
+			double s = MesquiteDouble.fromString(parser.getFirstToken(arguments));
+			if (MesquiteDouble.isCombinable(s)){
+				siteGappinessThreshold = s;
+				if (!MesquiteThread.isScripting())
+					parametersChanged(); 
+
+			}
+		}
+		else if (checker.compare(this.getClass(), "Sets proportion of sites in span that are gappy to trigger selection.", "[on or off]", commandName, "setBlockGappinessThreshold")) {
+			double s = MesquiteDouble.fromString(parser.getFirstToken(arguments));
+			if (MesquiteDouble.isCombinable(s)){
+				blockGappinessThreshold = s;
+				if (!MesquiteThread.isScripting())
+					parametersChanged(); 
+
 			}
 		}
 
@@ -102,9 +186,14 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 	public String preparePreferencesForXML () {
 		StringBuffer buffer = new StringBuffer(200);
 		StringUtil.appendXMLTag(buffer, 2, "proportionIncompat", proportionIncompat);  
-		StringUtil.appendXMLTag(buffer, 2, "spanSize", spanSize);  
+		StringUtil.appendXMLTag(buffer, 2, "spanSize", blockSize);  
 		StringUtil.appendXMLTag(buffer, 2, "neighbourDistance", neighbourDistance);  
 		StringUtil.appendXMLTag(buffer, 2, "treatGapAsState", treatGapAsState);  
+		StringUtil.appendXMLTag(buffer, 2, "filterGappiness", filterGappiness);  
+		StringUtil.appendXMLTag(buffer, 2, "siteGappinessThreshold", siteGappinessThreshold);  
+		StringUtil.appendXMLTag(buffer, 2, "minGappyBlockSize", minGappyBlockSize);  
+		StringUtil.appendXMLTag(buffer, 2, "blockGappinessThreshold", blockGappinessThreshold);  
+		StringUtil.appendXMLTag(buffer, 2, "minGappyBoundary", minGappyBoundary);  
 
 		return buffer.toString();
 	}
@@ -117,11 +206,22 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 		if ("proportionIncompat".equalsIgnoreCase(tag))
 			proportionIncompat = MesquiteDouble.fromString(content);
 		if ("spanSize".equalsIgnoreCase(tag))
-			spanSize = MesquiteInteger.fromString(content);
+			blockSize = MesquiteInteger.fromString(content);
 		if ("neighbourDistance".equalsIgnoreCase(tag))
 			neighbourDistance = MesquiteInteger.fromString(content);
 		if ("treatGapAsState".equalsIgnoreCase(tag))
 			treatGapAsState.setValue(MesquiteBoolean.fromTrueFalseString(content));
+		
+		if ("blockGappinessThreshold".equalsIgnoreCase(tag))
+			blockGappinessThreshold = MesquiteDouble.fromString(content);
+		if ("siteGappinessThreshold".equalsIgnoreCase(tag))
+			siteGappinessThreshold = MesquiteDouble.fromString(content);
+		if ("minGappyBlockSize".equalsIgnoreCase(tag))
+			minGappyBlockSize = MesquiteInteger.fromString(content);
+		if ("minGappyBoundary".equalsIgnoreCase(tag))
+			minGappyBoundary = MesquiteInteger.fromString(content);
+		if ("filterGappiness".equalsIgnoreCase(tag))
+			filterGappiness.setValue(MesquiteBoolean.fromTrueFalseString(content));
 
 	}
 
@@ -131,18 +231,32 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 		MesquiteInteger buttonPressed = new MesquiteInteger(1);
 		ExtensibleDialog dialog = new ExtensibleDialog(mb.containerOfModule(),  "Select using PhyIN Algorithm",buttonPressed);  //MesquiteTrunk.mesquiteTrunk.containerOfModule()
 
-		IntegerField SSField = dialog.addIntegerField("Length of blocks (-b)", spanSize, 4);
+		dialog.addLabel("PhyIN criteria for incompatible sites:");
+		IntegerField SSField = dialog.addIntegerField("Length of blocks (-b)", blockSize, 4);
 		IntegerField NDField = dialog.addIntegerField("Distance surveyed for conflict among neighbours (-d)", neighbourDistance, 4);
 		DoubleField PIField = dialog.addDoubleField("Proportion of neighbouring sites in conflict to trigger block selection (-p)", proportionIncompat, 4);
-		Checkbox tGAS = dialog.addCheckBox("Treat non-terminal gaps as extra state (-e)", treatGapAsState. getValue());
+		Checkbox tGAS = dialog.addCheckBox("Treat non-terminal gaps as extra state (-e)", treatGapAsState.getValue());
+
+		dialog.addHorizontalLine(1);
+		Checkbox fG = dialog.addCheckBox("Filter also by gappiness", filterGappiness.getValue());
+		DoubleField pgSField = dialog.addDoubleField("Proportion of gaps that marks site as bad", siteGappinessThreshold, 4);
+		IntegerField minGBS = dialog.addIntegerField("Minimum length of bad block", minGappyBlockSize, 4);
+		IntegerField minGB = dialog.addIntegerField("Stretch of good that resets block", minGappyBoundary, 4);
+		DoubleField pgBField = dialog.addDoubleField("Proportion of bad sites for block to be bad", blockGappinessThreshold, 4);
 
 
 		dialog.completeAndShowDialog(true);
 		if (buttonPressed.getValue()==0)  {
 			proportionIncompat = PIField.getValue();
-			spanSize = SSField.getValue();
+			blockSize = SSField.getValue();
 			neighbourDistance = NDField.getValue();
 			treatGapAsState.setValue(tGAS.getState());
+
+			filterGappiness.setValue(fG.getState());
+			siteGappinessThreshold = pgSField.getValue();
+			minGappyBlockSize = minGBS.getValue();
+			minGappyBoundary = minGB.getValue();
+			blockGappinessThreshold = pgBField.getValue();
 			storePreferences();
 		}
 		dialog.dispose();
@@ -229,6 +343,7 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 		}
 		return false;
 	}
+
 	/*.................................................................................................................*/
 	boolean areIncompatible(CategoricalData data, int ic, int ic2) {
 		if (ic>=data.getNumChars() || ic <0 || ic2>=data.getNumChars() || ic2 <0)
@@ -327,14 +442,84 @@ public class PhyINSelector extends CharacterSelectorPersistent {
 
 	}
 
+
+	/*.................................................................................................................*/
+	//This is an auxilliary criterion, here just so that it can assess in the pre-trimmed fullness of the alignment
+	double[] siteGappiness;
+	boolean isGapBlockBoundary(int k) {
+		int i = 0;
+		for (i = 0; k+i<siteGappiness.length && i<minGappyBoundary; i++)
+			if (siteGappiness[k+i]> siteGappinessThreshold)
+				return false;
+		return true;
+	}
+	boolean gappySite(int k) {
+		return siteGappiness[k]>siteGappinessThreshold;
+	}
+	void markGappyBlocks(CategoricalData data) {
+		int numChars = data.getNumChars();
+		int numTaxa = data.getNumTaxa();
+		if (siteGappiness == null || siteGappiness.length != numChars) {
+			siteGappiness = new double[numChars];
+		}
+		for (int ic=0; ic<numChars; ic++) {
+			int gapCount = 0;
+			for (int it = 0; it<numTaxa; it++) {
+				if (taxonSequenceStart[it]>=0 && data.isInapplicable(ic,it)) //if taxonSequenceStart < 0 then taxon has no data
+					gapCount++;
+			}
+			siteGappiness[ic] = 1.0*gapCount/numTaxa;
+		}
+
+		for (int blockStart=0; blockStart<numChars; blockStart++) { //let's look for gappy blocks
+			if (gappySite(blockStart)) { // possible start of gappy block
+
+				boolean boundaryFound = false;
+				for (int candidateNextBoundary = blockStart+1; candidateNextBoundary<numChars+1 && !boundaryFound; candidateNextBoundary++) {  // go ahead until next boundary reached
+					if (isGapBlockBoundary(candidateNextBoundary) || candidateNextBoundary == numChars) {
+						boundaryFound = true;
+						int blockEnd = candidateNextBoundary-1;
+
+						//blockStart is the potential start of a block; blockEnd is a possible end. If the block is long enough, ask if its blockGappiness is bad
+						if (blockEnd-blockStart+1 >= minGappyBlockSize){
+							//block is big enough, but is it bad enough?
+							int badSiteCount = 0;
+							for (int k = blockStart; k <= blockEnd; k++)
+								if (gappySite(k))  // stored as double[] in case criterion shifts, e.g., to average
+									badSiteCount++;
+							double blockGappiness = 1.0*badSiteCount/(blockEnd-blockStart+1);
+							if (blockGappiness >blockGappinessThreshold)
+								for (int k = blockStart; k <= blockEnd; k++)
+									toSelect[k] = true;
+
+
+						}
+
+					}
+				}
+			}
+		}
+
+
+	}
+
 	/*.................................................................................................................*/
 	/** Called to select characters*/
 	public void selectCharacters(CharacterData data){
 		if (data!=null && data.getNumChars()>0 && data instanceof CategoricalData){
+				//First, use PhyIn criteria to select by incompatibility
+			
 			markThoseWithConflict((CategoricalData)data);
 			for (int ic=0; ic<hasConflict.length; ic++) {
-				selectSpanByProportion(ic, hasConflict, toSelect, proportionIncompat, spanSize);
+				selectSpanByProportion(ic, hasConflict, toSelect, proportionIncompat, blockSize);
 			}
+		
+			//Second, select by gappiness if desired
+			if (filterGappiness.getValue())
+				markGappyBlocks((CategoricalData)data);
+
+
+
 			for (int ic=0; ic<data.getNumChars(); ic++) {
 				if (toSelect[ic])
 					data.setSelected(ic, true);
