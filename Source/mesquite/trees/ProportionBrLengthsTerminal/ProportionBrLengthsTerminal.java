64ede780b90710f6b48117371ff5fc87c6970b46
wmaddisn <wmaddisn@192.168.1.71>
2024-07-10 15:17:26 -0700

    Add proportion branch lengths terminal

---
+++ Source/mesquite/trees/ProportionBrLengthsTerminal/ProportionBrLengthsTerminal.java
@@ -0,0 +1,97 @@
+/* Mesquite source code.  Copyright 1997 and onward, W. Maddison and D. Maddison. 
+
+
+Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
+The commenting leaves much to be desired. Please approach this source code with the spirit of helping out.
+Perhaps with your help we can be more than a few, and make Mesquite better.
+
+Mesquite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+Mesquite's web site is http://mesquiteproject.org
+
+This source code and its compiled class files are free and modifiable under the terms of 
+GNU Lesser General Public License.  (http://www.gnu.org/copyleft/lesser.html)
+ */
+package mesquite.trees.ProportionBrLengthsTerminal;
+
+import mesquite.lib.*;
+import mesquite.lib.duties.*;
+
+/** this is a silly little module that can be used as a demonstration for NumberForTree modules */
+public class ProportionBrLengthsTerminal extends NumberForTree {
+	MesquiteNumber nt;
+	/*.................................................................................................................*/
+	public boolean startJob(String arguments, Object condition, boolean hiredByName) {
+		nt= new MesquiteNumber();
+		return true;
+	}
+	/*.................................................................................................................*/
+	public boolean isSubstantive(){
+		return true;
+	}
+	/*.................................................................................................................*/
+	public boolean isPrerelease(){
+		return true;
+	}
+	/*.................................................................................................................*/
+	/** returns the version number at which this module was first released.  If 0, then no version number is claimed.  If a POSITIVE integer
+	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
+	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
+	public int getVersionOfFirstRelease(){
+		return NEXTRELEASE;  
+	}
+	/*-----------------------------------------*/
+	public void surveyBranches(Tree tree, int node, MesquiteDouble termBL, MesquiteDouble totalBL) {
+		double length = tree.getBranchLength(node);
+		if (tree.getRoot() != node && MesquiteDouble.isCombinable(length)){
+
+			if (!totalBL.isCombinable())
+				totalBL.setValue(length);
+			else
+				totalBL.add(length);
+			if (tree.nodeIsTerminal(node)){
+				if (!termBL.isCombinable())
+					termBL.setValue(length);
+				else
+					termBL.add(length);
+			}
+
+		}
+		for (int d = tree.firstDaughterOfNode(node); tree.nodeExists(d); d = tree.nextSisterOfNode(d))
+			surveyBranches(tree, d, termBL, totalBL);
+
+	}
+	/*.................................................................................................................*/
+	public void calculateNumber(Tree tree, MesquiteNumber result, MesquiteString resultString) {
+		if (result==null || tree==null)
+			return;
+		clearResultAndLastResult(result);
+		MesquiteDouble termBL = new MesquiteDouble();
+		MesquiteDouble totalBL = new MesquiteDouble();
+		surveyBranches(tree, tree.getRoot(), termBL, totalBL);
+		if (termBL.isCombinable() && totalBL.isCombinable() && totalBL.getValue() != 0){
+			nt.setValue(termBL.getValue()/totalBL.getValue());
+			result.setValue(nt);
+			if (resultString!=null)
+				resultString.setValue("Ratio: "+ nt.toString());
+		}
+		saveLastResult(result);
+		saveLastResultString(resultString);
+	}
+	/*.................................................................................................................*/
+	/*.................................................................................................................*/
+	public String getName() {
+		return "Proportion Terminal Branch Lengths";
+	}
+	public String getNameForMenuItem() {
+		return "Prop. Terminal Branch Lengths";
+	}
+	/*.................................................................................................................*/
+	/*.................................................................................................................*/
+	/*.................................................................................................................*/
+	/** returns an explanation of what the module does.*/
+	public String getExplanation() {
+		return "Gets the proportion of branch length in a tree that is on terminal branches." ;
+	}
+
+}
+
