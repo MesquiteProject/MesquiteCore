44b0e48ad722aa7a8179407c5d493734fc72317d
David Maddison <david.maddison@science.oregonstate.edu>
2025-03-13 16:27:13 -0700

    Two tools relevant for GenBank submission

---
+++ Source/mesquite/molec/FindInternalGap/FindInternalGap.java
@@ -0,0 +1,86 @@
+/* Mesquite source code.  Copyright 1997 and onward, W. Maddison and D. Maddison. 
+
+
+Disclaimer:  The Mesquite source code is lengthy and we are few.  There are no doubt inefficiencies and goofs in this code. 
+The commenting leaves much to be desired. Please approach this source code with the spirit of helping out.
+Perhaps with your help we can be more than a few, and make Mesquite better.
+
+Mesquite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+Mesquite's web site is http://mesquiteproject.org
+
+This source code and its compiled class files are free and modifiable under the terms of 
+GNU Lesser General Public License.  (http://www.gnu.org/copyleft/lesser.html)
+*/
+package mesquite.molec.FindInternalGap;
+/*~~  */
+
+import mesquite.categ.lib.DNAData;
+import mesquite.categ.lib.DNAState;
+import mesquite.lib.*;
+import mesquite.lib.characters.*;
+import mesquite.lib.table.*;
+import mesquite.molec.lib.*;
+
+/* ======================================================================== */
+public class FindInternalGap extends FindSequenceCriterionG {
+	/*.................................................................................................................*/
+	public boolean startJob(String arguments, Object condition, boolean hiredByName) {
+		return true;
+	}
+	/*.................................................................................................................*/
+	public boolean isPrerelease(){
+		return true;
+	}
+	/*.................................................................................................................*/
+	/** returns whether this module is requesting to appear as a primary choice */
+	public boolean requestPrimaryChoice(){
+		return false;  
+	}
+	public boolean showOptions(CharacterData data, MesquiteTable table){
+		return true;
+	}
+	public boolean findNext(CharacterData data, MesquiteTable table, MesquiteInteger charFound, MesquiteInteger length, MesquiteInteger taxonFound) {
+		length.setValue(0);
+		int firstTaxon = taxonFound.getValue();
+		int firstChar = charFound.getValue();
+		int numTaxa = data.getNumTaxa();
+		int numChars = data.getNumChars();
+		for (int it = firstTaxon; it< numTaxa; it++){
+			for (int ic = firstChar; ic< numChars; ic++) {
+				if (data.isInternalInapplicable(ic, it)) {
+					charFound.setValue(ic);
+					taxonFound.setValue(it);
+					int next=data.nextApplicable(it, ic, true);
+					length.setValue(next-ic);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+
+	/*.................................................................................................................*/
+	public String getName() {
+		return "Find Internal Gaps";
+	}
+	/*.................................................................................................................*/
+	/** returns an explanation of what the module does.*/
+	public String getExplanation() {
+		return "Finds the next occurrence of an internal gap in a matrix of molecular data." ;
+	}
+	public boolean showOptions(boolean findAll, CharacterData data, MesquiteTable table) {
+		return true;
+	}
+	
+	/*.................................................................................................................*/
+	/** returns the version number at which this module was first released.  If 0, then no version number is claimed.  If a POSITIVE integer
+	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
+	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
+	public int getVersionOfFirstRelease(){
+		return  NEXTRELEASE;  
+	}
+
+
+}
+
---
+++ Source/mesquite/molec/NumInternalMissingMatrix/NumInternalMissingMatrix.java
@@ -0,0 +1,78 @@
+package mesquite.molec.NumInternalMissingMatrix;
+
+
+import mesquite.lib.*;
+import mesquite.lib.characters.*;
+import mesquite.lib.duties.*;
+
+public class NumInternalMissingMatrix extends NumberForMatrix {
+
+	public boolean startJob(String arguments, Object condition, boolean hiredByName) {
+		return true;
+	} 
+
+	/** Called to provoke any necessary initialization.  This helps prevent the module's initialization queries to the user from happening at inopportune times (e.p., while a long chart calculation is in mid-progress*/
+	public void initialize(MCharactersDistribution data) {
+	} 
+
+	public void calculateNumber(MCharactersDistribution data, MesquiteNumber result, MesquiteString resultString) {
+		if (result == null || data == null)
+			return;
+		clearResultAndLastResult(result);
+
+		long valid = 0;
+		long total = 0;
+		CharacterData parentData = data.getParentData();
+		if (parentData == null){
+			if (resultString != null)
+				resultString.setValue("Number of internal gaps can be calculated only for stored matrices");
+			return;
+		}
+		int numTaxa = parentData.getNumTaxa();
+		int numChars = parentData.getNumChars();
+
+		int confirmedInternal = 0;
+		for (int it=0; it<numTaxa; it++){
+			boolean stateFound = false;
+			int countMissing = 0;
+					
+			for (int ic=0; ic<numChars; ic++){
+				if (parentData.isUnassigned(ic,it)){   // we have found a missing
+					if (stateFound)  //to the right of sequence, just possibly an internal missing
+						countMissing++;
+				}
+				else if (!parentData.isInapplicable(ic, it)){  // found a non-gap, non-missing
+					stateFound = true;
+					if (countMissing >0){ // some missing counting accumulated; consider them confirmed as internal
+						confirmedInternal += countMissing;
+						countMissing = 0;
+					}
+				}
+				total++;
+			}
+		}
+		if (confirmedInternal>0) {
+			result.setValue(confirmedInternal); 
+		}  else
+			result.setValue(0.0); 
+
+		if (resultString!=null) {
+			resultString.setValue("Number of internal missing data: " + result.toString());
+		}
+		saveLastResult(result);
+		saveLastResultString(resultString);
+	} 
+
+	public boolean isPrerelease (){
+		return false;
+	}
+
+	public String getName() {
+		return "Number of Internal Missing Data Entries in Matrix";
+	} 
+
+	public String getExplanation(){
+		return "Calculates the number of internal missing data entries in the matrix.";
+	} 
+
+} 
