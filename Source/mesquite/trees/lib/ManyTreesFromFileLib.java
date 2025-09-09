c5fee1dba6cfb8cedbb8e47b012985ac9c747f9e
wmaddisn <wmaddisn@mail.ubc.ca>
2025-09-08 19:30:13 -0700

    Protection against errors while closing.

--- Source/mesquite/trees/lib/ManyTreesFromFileLib.java
+++ Source/mesquite/trees/lib/ManyTreesFromFileLib.java
@@ -1201,6 +1201,8 @@ public abstract class ManyTreesFromFileLib extends TreeSource implements Mesquit
 		fileWasModified = true;
 		if (!live.getValue())
 			return;
+		if (isDoomed())
+			return;
 		fileWasModified = false;
 		int s = numTrees;
 		numTrees = MesquiteInteger.finite;
