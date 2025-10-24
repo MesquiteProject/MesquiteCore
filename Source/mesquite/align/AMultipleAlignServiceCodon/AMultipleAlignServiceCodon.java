4b0f0916a07b99b86e676b35028c93619bbbecaf
wmaddisn <wmaddisn@mail.ubc.ca>
2025-10-24 14:24:54 -0700

    Docs, release settings.

--- Source/mesquite/align/AMultipleAlignServiceCodon/AMultipleAlignServiceCodon.java
+++ Source/mesquite/align/AMultipleAlignServiceCodon/AMultipleAlignServiceCodon.java
@@ -66,7 +66,7 @@ public class AMultipleAlignServiceCodon extends AMultipleAlignServiceLib{
    	 * then the number refers to the Mesquite version.  This should be used only by modules part of the core release of Mesquite.
    	 * If a NEGATIVE integer, then the number refers to the local version of the package, e.g. a third party package*/
       	public int getVersionOfFirstRelease(){
-      		return NEXTRELEASE;  
+      		return 402;  
     }
     	/*.................................................................................................................*/
    	 public String getName() {
